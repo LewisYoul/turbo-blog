6ee97848e9ffebc532eb7b9dac0b17c78be64949
Lewis Youl <lewis.youl@trustflight.io>
2021-09-04 07:57:04 +0100

    feat: use post title as page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def title
+    content_for?(:title) ? content_for(:title) : "Lewis Youl"
+  end
+
+  def twitter_card
+    content_for(:twitter_card) if content_for?(:twitter_card)
+  end
 end
