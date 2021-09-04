cfc59853257b038faf85112df9972796979e7ea6
Lewis Youl <lewis.youl@trustflight.io>
2021-09-04 08:01:54 +0100

    feat: add default twitter card

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,18 @@ module ApplicationHelper
   end
 
   def twitter_card
-    content_for(:twitter_card) if content_for?(:twitter_card)
+    if content_for?(:twitter_card)
+      content_for(:twitter_card)
+    else
+      raw(
+        <<-HTML
+          <meta name="twitter:card" content="summary">
+          <meta name="twitter:title" content="Lewis Youl">
+          <meta name="twitter:description" content="A web development blog by some fella that lives in blighty">
+          <meta name="twitter:image" content="https://miro.medium.com/max/1420/1*9hd_8qR0CMZ8L0pVbFLjDw.png">
+          <meta name="twitter:site" content="@lewisyoul">
+        HTML
+      )
+    end
   end
 end
