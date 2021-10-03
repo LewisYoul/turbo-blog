45a2e16abdeed4767d0d64495292867892d13cc4
Lewis Youl <lewis.youl@trustflight.io>
2021-10-03 17:57:20 +0100

    feat: add infinite scrolling to root page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,15 @@
 class HomeController < ApplicationController
+  PER_PAGE = 10.freeze
+  FIRST_PAGE = 1.freeze
+
   def index
-    @posts = Post.includes(:tags).order(created_at: :desc)
+    @page = params[:page]&.to_i || FIRST_PAGE
+    @next_page = @page + 1
+    offset = PER_PAGE * (@page - 1)
+    @posts = Post.includes(:tags)
+      .order(created_at: :desc)
+      .offset(offset)
+      .limit(PER_PAGE)
+    @has_next_page = @posts.size == PER_PAGE 
   end
 end
